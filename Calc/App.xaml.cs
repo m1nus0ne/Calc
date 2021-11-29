f47cb5b999b1d68c865b893e7c4608db82b84a33
m1nus0ne <voldemar7455@gmail.com>
2021-11-29 13:09:02 +0500

    init

---
+++ Calc/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using static Calc.TextWindow;
+
+namespace Calc
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        
+    }
+}
---
+++ Calc/TextWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Calc
+{
+    public partial class TextWindow : Window
+    {
+        public TextWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Update(string text)
+        {
+            TextBox.Text = text;
+        }
+    }
+}
